d70c15dc9a9dfe8423d45afa69544c07c68be4fd
daijianBANG <daijianbang@yeah.net>
2022-01-21 15:21:47 +0800

    update to mc 1.17

--- conn.go
+++ conn.go
@@ -1,35 +1,39 @@
 // 在 fb 对应文件(minecraft/conn.go)中添加以下两个函数
 
 func (conn *Conn) WritePacketBytes(data []byte) error {
-	conn.sendMutex.Lock()
-	defer conn.sendMutex.Unlock()
+	select {
+	case <-conn.close:
+		return conn.closeErr("write packet")
+	default:
+	}
+	conn.sendMu.Lock()
+	defer conn.sendMu.Unlock()
 
 	conn.bufferedSend = append(conn.bufferedSend, append([]byte(nil), data...))
-	conn.writeBuf.Reset()
 	return nil
 }
 
-func (conn *Conn) ReadPacketAndBytes() (k packet.Packet, data []byte, err error) {
-	if data, ok := conn.takePushedBackPacket(); ok {
-		pk, err := conn.parsePacket(data, false)
+func (conn *Conn) ReadPacketAndBytes() (pk packet.Packet, data []byte, err error) {
+	if data, ok := conn.takeDeferredPacket(); ok {
+		pk, err := data.decode(conn)
 		if err != nil {
 			conn.log.Println(err)
 			return conn.ReadPacketAndBytes()
 		}
-		return pk, data, nil
+		return pk, data.full, nil
 	}
 
 	select {
+	case <-conn.close:
+		return nil, nil, conn.closeErr("read packet")
+	case <-conn.readDeadline:
+		return nil, nil, conn.wrap(context.DeadlineExceeded, "read packet")
 	case data := <-conn.packets:
-		pk, err := conn.parsePacket(data, true)
+		pk, err := data.decode(conn)
 		if err != nil {
 			conn.log.Println(err)
 			return conn.ReadPacketAndBytes()
 		}
-		return pk, data, nil
-	case <-conn.readDeadline:
-		return nil, nil, fmt.Errorf("error reading packet: read timeout")
-	case <-conn.closeCtx.Done():
-		return nil, nil, fmt.Errorf("error reading packet: connection closed")
+		return pk, data.full, nil
 	}
 }
